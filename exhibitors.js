81c47ba9b1018730a53a07ebe0f44b7422b8ee02
hejnerhofflohmarkt <hejnerhofflohmarkt@gmail.com>
2025-08-06 14:24:55 +0200

    Add files via upload

---
+++ exhibitors.js
@@ -0,0 +1,22 @@
+// exhibitors.js
+// Beispielhafte Aussteller-Daten für den Hofflohmarkt
+
+const exhibitors = [
+    {
+        street: "Musterstraße",
+        number: "12",
+        name: "Familie Schmidt",
+        description: "Spielzeug, Kleidung, Bücher",
+        lat: 51.1657,
+        lng: 10.4515
+    },
+    {
+        street: "Beispielweg",
+        number: "8a",
+        name: "Anna und Tom",
+        description: "Antiquitäten, Möbel",
+        lat: 51.1667,
+        lng: 10.4530
+    }
+    // Weitere Einträge hier ergänzen ...
+];
